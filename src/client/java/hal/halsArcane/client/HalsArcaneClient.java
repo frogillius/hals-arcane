27497103d6678e8243d3f78e6eaec6e94a083314
halufun <frogillius@hotmail.com>
2024-12-24 11:27:16 -0800

    Edit for compatibility with and init of inventory tab.

--- src/client/java/hal/halsArcane/client/HalsArcaneClient.java
+++ src/client/java/hal/halsArcane/client/HalsArcaneClient.java
@@ -1,11 +1,13 @@
 package hal.halsArcane.client;
 
+import hal.halsArcane.client.screens.HalsArcaneOrbTabScreen;
+import hal.halsArcane.register.InventoryScreen;
 import net.fabricmc.api.ClientModInitializer;
+import net.minecraft.client.gui.screen.ingame.HandledScreens;
 
-public class HalsArcaneClient implements ClientModInitializer {
-
+public final class HalsArcaneClient implements ClientModInitializer {
     @Override
     public void onInitializeClient() {
+        HandledScreens.register(InventoryScreen.CUSTOM_TAB, HalsArcaneOrbTabScreen::new);
     }
 }
-
--- src/main/java/hal/halsArcane/HalsArcane.java
+++ src/main/java/hal/halsArcane/HalsArcane.java
@@ -1,10 +1,11 @@
 package hal.halsArcane;
 
+import hal.halsArcane.register.InventoryScreen;
 import net.fabricmc.api.ModInitializer;
 
 public class HalsArcane implements ModInitializer {
-
     @Override
     public void onInitialize() {
+        InventoryScreen.register();
     }
 }
